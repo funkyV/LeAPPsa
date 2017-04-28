14f69005dfa848bfcfbd7ead3b8c80e4ec11aaa5
Jitariuc <sebi.jitariuc@yahoo.ro>
2017-04-28 23:35:09 +0300

    update index.php
    adaugarea statisticilor si a butonului de login.

--- index.php
+++ index.php
@@ -9,13 +9,29 @@ require_once("/inc/header.php");
              <div>
 
                 <div style="width:200px;margin:2px;float:left;">
-                    
+                    <div method="post" style="width:300px;margin:0 auto;" enctype="multipart/form-data">
+                        <label for="members">Membri : 30</label>
+                        <br/>
+                        <label for="members">Intrebari : 20</label>
+                        <br/>
+                        <label for="members">Raspunsuri : 14</label>
+                        <br/>
+                    </div>
                 </div>
 
                 <div>
 
                     <div style="width:500px;margin:2px;float:left;">
-                        
+                        <div method="post" style="width:300px;margin:0 auto;" enctype="multipart/form-data">
+                            <label for="members">Ultimul membru inregistrat : florinel21</label>
+                            <br/>
+                            <label for="members">Ultima intrebare : #215b</label>
+                            <br/>
+                            <label for="members">Ultimul raspuns : #438c</label>
+                            <br/>
+                            <br/>
+                            <a href="login.php"><input type="submit" value="Login" style="width:150px"></a>
+                        </div>
                     </div>
 
                 </div>
