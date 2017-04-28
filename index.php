aec11a4fb6a338c1c402f891a4acf084fab49838
Jitariuc <sebi.jitariuc@yahoo.ro>
2017-04-28 23:30:42 +0300

    update index.php
    crearea structurii paginii

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
+<?php
+require_once("/inc/header.php");
+?>
+    <div id="site_content">
 
+        <div id="content">
+            <h1 class='center'>Bun venit!</h1>
+            <br/>
+         </div>
+      </div>
+<?
+require_once("footer.php");
+?>
