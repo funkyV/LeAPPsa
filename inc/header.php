3b67ce9fa7290ce6cc5bd360bc40f18abde40e31
vladminea <minea.vlad@gmail.com>
2017-04-29 02:02:56 +0300

    started header.php

---
+++ inc/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+      <title>LeAPPsa</title>
+      <meta name="description" content="website description" />
+      <meta name="keywords" content="website keywords, website keywords" />
+      <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+      <link rel="stylesheet" type="text/css" href="http://localhost/tw/LeAPPsa/style/style.css" title="style" />
+    </head>
+<body>
+</body>
+</html>
