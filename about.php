2f2f505ad15d6568ca4b28ad4ff3854f8ab565ff
Jitariuc <sebi.jitariuc@yahoo.ro>
2017-04-28 23:37:28 +0300

    update about.php
    initierea paginii

--- about.php
+++ about.php
@@ -1,1 +1,14 @@
+<?php
+require_once("/inc/header.php");
+?>
+
+    <div id="site_content">
+
+        <div id="content">
+
+            </div>
+    </div>
+<?php
+require_once("inc/footer.php");
+?>
 
