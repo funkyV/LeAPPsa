1ed944dc23e3d92ec92553212397b7471a335e56
Jitariuc <sebi.jitariuc@yahoo.ro>
2017-04-28 23:18:55 +0300

    update login.php
    tratarea cazului in care unul din campuri este gol

--- login.php
+++ login.php
@@ -3,11 +3,14 @@
      
       <div id="content">
        <form action="methods/login_process.php" method="post" style="width:300px;margin:0 auto;">
-                  
-                    <label for="email">Email:</label><input type="email" id="email" name="email">
+                   <?php
+                  if (!empty($_GET["err"]))
+                  echo $_GET["err"]."<br/>";
+                  ?>
+                   <label for="email">Email:</label><input type="email" id="email" name="email">
                   <br/>
-                    <label for="password">Password:</label>
-                    <input type="password" id="password" name="password">
+                  <label for="password">Password:</label>
+                  <input type="password" id="password" name="password">
                   <br/>
                   <a href="register.php">Nu ai cont? Inregistreaza-te!</a><br/>
                   <a href="register.php">Ai uitat parola?</a>
