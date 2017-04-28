f0376c25c5ad5740a70f2e33df59e77c9c6c5b47
Jitariuc <sebi.jitariuc@yahoo.ro>
2017-04-28 23:26:10 +0300

    update ask.php
    adaugarea formularului de adresare a intrebarii;
    adaugarea headerului si footerului.

--- ask.php
+++ ask.php
@@ -1,9 +1,19 @@
-
+<?php
+require_once("/inc/header.php");
+?>
 <div id="site_content">
 
         <div id="content">
         <h1 class="center" style="">Porneste Leapsa!</h1>
         <br/>
+        <form action="process/ask_process.php" method="post" class="center" style="width:400px;margin:0 auto;">
+                <input type="text" id="question" name="question">
+                <br/>
+                <input type="submit" value="Start Leapsa" style="width:150px">
+         </form>
         </div>
 
 </div>
+<?
+require_once("/inc/footer.php");
+?>
