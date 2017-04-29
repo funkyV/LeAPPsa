bf597881ff5683e53910c87c6a4003e2088c1aef
vladminea <minea.vlad@gmail.com>
2017-04-29 07:54:16 +0300

    fixed the path to header and footer in login.php; also validated it through w3 validator

--- login.php
+++ login.php
@@ -1,25 +1,29 @@
 <?php
-require_once("/inc/header.php");
+require_once("./inc/header.php");
 ?>
-<div id="site_content">
-     
-      <div id="content">
-       <form action="methods/login_process.php" method="post" style="width:300px;margin:0 auto;">
-                   <?php
-                  if (!empty($_GET["err"]))
-                  echo $_GET["err"]."<br/>";
-                  ?>
-                   <label for="email">Email:</label><input type="email" id="email" name="email">
-                  <br/>
-                  <label for="password">Password:</label>
-                  <input type="password" id="password" name="password">
-                  <br/>
-                  <a href="register.php">Nu ai cont? Inregistreaza-te!</a><br/>
-                  <a href="register.php">Ai uitat parola?</a>
-                  <input type="submit" value="Login">
-         </form>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+      <div id="site_content">
+        <div id="content">
+          <form style="width:300px;margin:0 auto;">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email">
+            <br/>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password">
+            <br/>
+            <a href="register.php">Nu ai cont? Inregistreaza-te!</a><br/>
+            <a href="register.php">Ai uitat parola?</a>
+            <input type="submit" value="Login">
+          </form>
+        </div>
       </div>
-</div>
+    </body>
+</html>
 <?php
-require_once("inc/footer.php");
+require_once("./inc/footer.php");
 ?>
