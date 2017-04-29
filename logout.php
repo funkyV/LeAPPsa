61d25624e85611b4475081d404848f3e4bb84616
vladminea <minea.vlad@gmail.com>
2017-04-29 07:32:19 +0300

    added register and logout

---
+++ logout.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Logout</title>
+    </head>
+    <body>
+        <label class='center'><h3><font color='#FF0000'>Ati fost deconectat!</font></h3></label>
+    </body>
+</html>
+<?php
+require_once("index.php");
+?>
---
+++ register.php
@@ -0,0 +1,36 @@
+<?php
+  require_once("./inc/header.php");
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Register</title>
+    </head>
+    <body>
+      <div id="site_content">
+      
+        <div id="content">
+          
+
+                  <form action="methods/register_process.php" method="post" style="width:300px;margin:0 auto;">
+                    <label for="username">Username:</label>
+                    <input type="text" id="username" name="username">
+                    <br/>
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email">
+                    <br/>
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password">
+                    <br/>
+                    <label for="repassword">Repeat Password:</label>
+                    <input type="password" id="repassword" name="repassword">
+                    <br/>
+                    <input type="submit" value="Register">
+                  </form>
+        </div>
+      </div>
+    </body>
+</html>
+<?
+  require_once("./inc/footer.php");
+?>
