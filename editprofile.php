a07ca8e48899ea7951f6c8d25df200e144fabee0
vladminea <minea.vlad@gmail.com>
2017-04-29 07:35:54 +0300

    added edit profile and profile

---
+++ editprofile.php
@@ -0,0 +1,42 @@
+<?php
+  require_once("./inc/header.php");
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Edit Profile</title>
+    </head>
+    <body>
+      <div id="site_content">
+        <div id="content" style="font-size:16px;">
+          <div style="width:170px;margin:2px;float:left;">
+            <img src="img/default.png" alt="" width="100" style="border-radius:50%;margin-left:35px;border:1px solid #323232">
+          </div>
+          <div style="width:370px;margin:2px;float:left;">
+            <form action="methods/update_process.php" method="post" style="width:300px;margin:0 auto;" enctype="multipart/form-data"> 
+              <label for="username">Username:</label>
+              <input type="text" id="username" name="username">">
+              <br/>
+              <label for="nume">Nume:</label>
+              <input type="text" id="nume" name="nume">
+              <br/>
+              <label for="prenume">Prenume:</label>
+              <input type="text" id="prenume" name="prenume">
+              <br/>
+              <label for="email">Email:</label><input type="email" id="email" name="email">
+              <br/>
+              <label for="avatar">Avatar:</label>
+              <input type="file" id="avatar" name="avatar">
+              <br/>
+              <input type="submit" value="Update">
+            </form>
+          </div>
+          <div style="width:270px;margin:2px;float:left;">
+          </div>      
+        </div>
+      </div>
+    </body>
+</html>
+<?php
+  require_once("./inc/footer.php");
+?>
---
+++ profile.php
@@ -0,0 +1,32 @@
+<?php
+  require_once("./inc/header.php");
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Profile</title>
+    </head>
+    <body>
+      <div id="site_content">
+        <div id="content" style="font-size:16px;">
+          <div style="width:170px;margin:2px;float:left;">
+            <img src="img/default.png" alt="" width="100" style="border-radius:50%;margin-left:35px;border:1px solid #323232">
+            <div style="border:1px solid #323232; margin:5px;min-height:100px;">
+              STATISTICI
+            </div>
+          </div>
+          <div style="width:370px;margin:2px;float:left;">
+            Username : <strong><u>Some username</u></strong><br/>
+            Nume : <strong><u>Popescu</u></strong><br/>
+            Prenume : <strong><u>Ion</u></strong><br/>
+            Email : <strong><u>popescu.ion@ionescu.com</u></strong><br/>
+          </div>
+          <div style="width:270px;margin:2px;float:left;">
+          </div>      
+        </div>
+      </div>
+    </body>
+</html>
+<?php
+  require_once("./inc/footer.php");
+?>
