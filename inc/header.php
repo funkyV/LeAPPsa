1a6f7bcc4d0fb216eab0c5f860db57204deff457
vladminea <minea.vlad@gmail.com>
2017-04-29 08:11:40 +0300

    changed the path to style.css in header.php

--- inc/header.php
+++ inc/header.php
@@ -5,7 +5,7 @@
       <meta name="description" content="website description" />
       <meta name="keywords" content="website keywords, website keywords" />
       <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-      <link rel="stylesheet" type="text/css" href="http://localhost/tw/LeAPPsa/style/style.css" title="style" />
+      <link rel="stylesheet" type="text/css" href="./style/style.css" title="style" />
     </head>
 <body>
     <div id="main">
