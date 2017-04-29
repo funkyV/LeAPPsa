08e30316d7fe6215c35b7c288460b930048432bb
vladminea <minea.vlad@gmail.com>
2017-04-29 07:49:27 +0300

    added doctype, html, head,title and body tags to index.php; also validated it through w3 validator

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 require_once("./inc/header.php");
 ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>LeAPPsa</title>
+</head>
+<body>
     <div id="site_content">
 
         <div id="content">
@@ -9,12 +15,12 @@ require_once("./inc/header.php");
              <div>
 
                 <div style="width:200px;margin:2px;float:left;">
-                    <div method="post" style="width:300px;margin:0 auto;" enctype="multipart/form-data">
-                        <label for="members">Membri : 30</label>
+                    <div style="width:300px;margin:0 auto;">
+                        <label>Membri : 30</label>
                         <br/>
-                        <label for="members">Intrebari : 20</label>
+                        <label>Intrebari : 20</label>
                         <br/>
-                        <label for="members">Raspunsuri : 14</label>
+                        <label>Raspunsuri : 14</label>
                         <br/>
                     </div>
                 </div>
@@ -22,15 +28,17 @@ require_once("./inc/header.php");
                 <div>
 
                     <div style="width:500px;margin:2px;float:left;">
-                        <div method="post" style="width:300px;margin:0 auto;" enctype="multipart/form-data">
-                            <label for="members">Ultimul membru inregistrat : florinel21</label>
+                        <div style="width:300px;margin:0 auto;">
+                            <label>Ultimul membru inregistrat : florinel21</label>
                             <br/>
-                            <label for="members">Ultima intrebare : #215b</label>
+                            <label>Ultima intrebare : #215b</label>
                             <br/>
-                            <label for="members">Ultimul raspuns : #438c</label>
+                            <label>Ultimul raspuns : #438c</label>
                             <br/>
                             <br/>
-                            <a href="login.php"><input type="submit" value="Login" style="width:150px"></a>
+                            <form action="login.php">
+                                <input type="submit" value="Login" style="width:150px" />
+                            </form>
                         </div>
                     </div>
 
@@ -40,6 +48,8 @@ require_once("./inc/header.php");
 
          </div>
       </div>
+</body>
+</html>
 <?
 require_once("./inc/footer.php");
 ?>
