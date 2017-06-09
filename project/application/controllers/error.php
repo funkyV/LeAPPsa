4372d7c252c03c356b458c3df9a0db600bed7145
SebiJ <sebi.jitariuc@yahoo.ro>
2017-06-09 10:53:42 +0300

    Am facut titlul din header dinamic; am corectat calea spre fisierul error.php

--- project/application/controllers/error.php
+++ project/application/controllers/error.php
@@ -6,7 +6,7 @@ class Errur extends Controller
     public function index()
     {
         require APP . 'views/_templates/header.php';
-        require APP . 'views/error/error.php';
+        require APP . 'views/error.php';
         require APP . 'views/_templates/footer.php';
     }
 }
