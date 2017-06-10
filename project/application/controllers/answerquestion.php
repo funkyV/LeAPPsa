aff778203a0f4971a9f70f8f29bab6ddd8a65e0e
vladminea <minea.vlad@gmail.com>
2017-06-10 16:45:33 +0300

    am inceput answer question

---
+++ project/application/controllers/answerquestion.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: funkyV
+ * Date: 10/06/2017
+ * Time: 15:30
+ */
+
+class Answerquestion extends Controller {
+    public function index() {
+        require HEADER;
+        require APP . '/views/answer.php';
+        require FOOTER;
+    }
+
+    public function id($questionId) {
+        $sql = 'S'
+    }
+
+
+
+
+}
