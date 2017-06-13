ecf4d7116f51a104e211c2f017f8e34c651f822f
SebiJ <sebi.jitariuc@yahoo.ro>
2017-06-13 11:33:39 +0300

    Am creat functia login in user

--- project/application/libs/user.php
+++ project/application/libs/user.php
@@ -13,5 +13,42 @@ class USER
       header('location: ' . URL_WITH_INDEX_FILE . $url);
    }
 
+   public function login($uname,$umail,$upass)
+    {
+        
+       try
+       {
+          $stmt = $this->db->prepare("SELECT * FROM users WHERE username=:uname OR email=:umail LIMIT 1");
+          $stmt->execute(array(':uname'=>$uname, ':umail'=>$umail));
+          $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+          
+          if($stmt->rowCount() > 0)
+          {
+             if(password_verify($upass, $userRow['password']))
+             {
+                $_SESSION['user_session'] = $userRow['id'];
+                return true;
+             }
+             else
+             {
+                return false;
+             }
+          }
+       }
+       catch(PDOException $e)
+       {
+           echo $e->getMessage();
+       }
+   }
+ 
+   public function is_loggedin()
+   {
+      if(isset($_SESSION['user_session']))
+      {
+         return true;
+      }else
+      return false;
+   }
+
 }
 ?>
