ac65cdf496db85e94aada878a5176b2c5b9452e2
SebiJ <sebi.jitariuc@yahoo.ro>
2017-06-13 15:07:41 +0300

    FIX: am reparat bug-ul din editProfile
    FIX: daca ramaneau campuri necompletate, stergea din baza de date valorile respective;
    acum username-ul este CaSe SeNsItIvE

--- project/application/controllers/editProfile.php
+++ project/application/controllers/editProfile.php
@@ -9,27 +9,27 @@ class editProfile extends Controller
     }
     public function process() {
     if(isset($_POST['edit_submit'])){
-        if(isset($_POST["username"])){
+        if(isset($_POST["username"]) && !empty($_POST["username"])){
             if($this->user->updateDetails($_SESSION['user_session'], 'username', $_POST["username"])){
                 $this->user->redirect(profil);
             }
         }
-        if(isset($_POST["firstname"])){
+        if(isset($_POST["firstname"]) && !empty($_POST["firstname"])){
             if($this->user->updateDetails($_SESSION['user_session'], 'firstname', $_POST["firstname"])){
                 $this->user->redirect(profil);
             }
         }
-        if(isset($_POST["lastname"])){
+        if(isset($_POST["lastname"]) && !empty($_POST["lastname"])){
             if($this->user->updateDetails($_SESSION['user_session'], 'lastname', $_POST["lastname"])){
                 $this->user->redirect(profil);
             }
         }
-        if(isset($_POST["email"])){
+        if(isset($_POST["email"])  && !empty($_POST["email"])){
             if($this->user->updateDetails($_SESSION['user_session'], 'email', $_POST["email"])){
                 $this->user->redirect(profil);
             }
         }      
-        if(isset($_POST["avatar"])){
+        if(isset($_POST["avatar"])  && !empty($_POST["avatar"])){
             if($this->user->updateDetails($_SESSION['user_session'], 'profile_pic', $_POST["avatar"])){
                 $this->user->redirect(profil);
             }
