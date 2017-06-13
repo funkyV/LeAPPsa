6617b6d0b8be614aa2c6f472d795f3511c9621c2
SebiJ <sebi.jitariuc@yahoo.ro>
2017-06-13 12:10:58 +0300

    am adaugat logout

--- project/application/controllers/logout.php
+++ project/application/controllers/logout.php
@@ -3,6 +3,7 @@
 class logout extends Controller
 {
     public function index() {
+        $this->user->logout();
         require HEADER;
         require APP . '/views/logout.php';
         require FOOTER;
