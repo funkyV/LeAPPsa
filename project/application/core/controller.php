473ecc4b3d754093319ccc2761ac7397854db473
vladminea <minea.vlad@gmail.com>
2017-06-17 04:57:16 +0300

    added a random string generator

--- project/application/core/controller.php
+++ project/application/core/controller.php
@@ -33,4 +33,14 @@ class Controller extends REST {
         $messageArray = array ('status' => $status, 'message' => $message);
         return $messageArray;
     }
+
+    function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
 }
