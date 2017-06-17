7afbaaf8489ed20edecd23384443cd8a91c82a0c
vladminea <minea.vlad@gmail.com>
2017-06-17 08:07:07 +0300

    ramane de construit arborele in bd

--- project/application/views/answeredQuestion.php
+++ project/application/views/answeredQuestion.php
@@ -10,9 +10,9 @@
 <div id="site_content">
 
     <div id="content">
-        <h1 class = "center">Felicitari pentru raspuns</h1>
+        <h1 class = "center"><?php echo ($this->question['question']);?></h1>
 
-        <h4 class = "center"> Acum poti da LeAPPsa mai departe</h4>
+        <h4 class = "center">Felicitari pentru raspuns!<br/> Acum poti da LeAPPsa mai departe.</h4>
 
 <ul class = "center" style = "list-style-type:none;">
     <?php
@@ -21,38 +21,21 @@
 //        echo '<li>' . $anEmail . '</li>';
 //    }
     ?>
-<!--    <form action="ask/askQuestion" method="POST" class="center" style="width:400px;margin:0 auto;">-->
-<!--        <label name = "question_types"> Question Types </label>-->
-<!--        <br/>-->
-<!--        <input list="question-types" name="question_types">-->
-<!--        <datalist id="question-types" multiple>-->
-<!--            <option value="Type1">-->
-<!--            <option value="Type2">-->
-<!--            <option value="Type3">-->
-<!--            <option value="Type4">-->
-<!--            <option value="Type5">-->
-<!--        </datalist>-->
-<!--        </input>-->
-<!--        <br/>-->
-<!--        <label name = "question"> Intrebarea </label>-->
-<!--        <br />-->
-<!--        <input type="text" id="question" name="question">-->
-<!--        <br/>-->
-<!---->
-<!---->
-<!--        <select style="width:200px; height:100px; text-align:center" name="emails[]" multiple>-->
-<!--            --><?php //foreach ($this->emailList as $email) {
-//                echo '<option value="' . $email['email'] . '">' . $email['email'] . '</option>';
-//            }
-//            ?>
-<!--            <option value="volvo">Volvo</option>-->
-<!--            <option value="saab">Saab</option>-->
-<!--            <option value="opel">Opel</option>-->
-<!--            <option value="audi">Audi</option>-->
-<!--        </select>-->
-<!--        <br/>-->
-<!--        <input type="submit" value="Start Leapsa" style="width:150px">-->
-<!--    </form>-->
+    <form action="ask/askQuestion" method="POST" class="center" style="width:400px;margin:0 auto;">
+        <br/>
+        <label name = "question"> Lista cu utilizatori "liberi"</label>
+        <br/>
+        <br/>
+        <select style="width:300px; height:200px; text-align:center" name="emails[]" multiple>
+            <?php foreach ($this->emailList as $email) {
+                echo '<option value="' . $email['email'] . '">' . $email['email'] . '</option>';
+            }
+            ?>
+        </select>
+        <br/>
+        <br/>
+        <input type="submit" value="Da LeAPPsa mai departe" style="width:150px">
+    </form>
 </ul>
 </div>
 </div>
